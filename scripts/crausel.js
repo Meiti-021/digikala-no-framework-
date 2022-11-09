41e445507e3e0170144f9c8cdd4dabfdb54c06eb
Mahdi <meiti.edu@gmail.com>
2022-11-09 21:48:31 +0330

    add offers crausel slider

---
+++ scripts/crausel.js
@@ -0,0 +1,35 @@
+const slider = document.querySelector('.crausel-container');
+
+
+
+let pressed = false;
+let startX;
+let scrollLeft;
+
+
+slider.scrollLeft = slider.scrollWidth;
+
+slider.addEventListener('mousedown', (e) => {
+    pressed = true;
+    startX = e.pageX - slider.offsetLeft; 
+    scrollLeft = slider.scrollLeft;
+})
+
+slider.addEventListener('mouseleave', () => {
+    pressed = false;
+})
+slider.addEventListener('mouseup', () => {
+    pressed = false;
+})
+slider.addEventListener('mousemove', (e) => {
+    if(!pressed) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x - startX;
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(slider.scrollLeft);
+})
+
+
+
+
