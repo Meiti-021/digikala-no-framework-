b110adf969502f454e3009139e547c491f9e2d5e
Mahdi <meiti.edu@gmail.com>
2022-11-12 02:45:57 +0330

    complete the  main section of mobile version including trends, commercials, digiplus, digiclub, dicountes, brands, posts ,populars an so on

--- scripts/crausel.js
+++ scripts/crausel.js
@@ -1,33 +1,32 @@
-const slider = document.querySelector('.crausel-container');
+const slideroner = document.querySelectorAll('.crausel-container');
 
 
 
-let pressed = false;
-let startX;
-let scrollLeft;
+slideroner.forEach((slider)=>{
+    let pressed = false;
+    let startX;
+    let scrollLeft;
 
-
-slider.scrollLeft = slider.scrollWidth;
-
-slider.addEventListener('mousedown', (e) => {
-    pressed = true;
-    startX = e.pageX - slider.offsetLeft; 
-    scrollLeft = slider.scrollLeft;
-})
-
-slider.addEventListener('mouseleave', () => {
-    pressed = false;
-})
-slider.addEventListener('mouseup', () => {
-    pressed = false;
-})
-slider.addEventListener('mousemove', (e) => {
-    if(!pressed) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = x - startX;
-    slider.scrollLeft = scrollLeft - walk;
-    console.log(slider.scrollLeft);
+    slider.scrollLeft = slider.scrollWidth;
+    slider.addEventListener('mousedown', (e) => {
+        pressed = true;
+        startX = e.pageX - slider.offsetLeft; 
+        scrollLeft = slider.scrollLeft;
+    })
+    slider.addEventListener('mouseleave', () => {
+        pressed = false;
+    })
+    slider.addEventListener('mouseup', () => {
+        pressed = false;
+    })
+    slider.addEventListener('mousemove', (e) => {
+        if(!pressed) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = x - startX;
+        slider.scrollLeft = scrollLeft - walk;
+        console.log(slider.scrollLeft);
+    })
 })
 
 
